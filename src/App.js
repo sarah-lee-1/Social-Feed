683e84c677c6d9e82a4d96dad2d44dda488b7e45
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-05 16:07:26 -0600

    Add Table format and initial data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const
-  
+  const [entries, setEntries] = useState([{Index: '', name: 'Buddy the Elf', post: 'Merry Christmas'}, {index: '', name: 'Scrooge', post: 'Bah Humbug'}])
+
   return (
     <div>
-      <h3>Hello World!</h3>
+      <table>
+        <thead>
+         <tr>
+           <th>Post#</th>
+           <th>Name:</th>
+           <th>Post:</th>
+           </tr> 
+        </thead>
+        <tbody>
+          {entries.map((entry, index) =>{
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{entry.name}</td>
+                <td>{entry.post}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+
+      </table>
     </div>
   );
 }
