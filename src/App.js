442d4f8b471917b2c75a70ba1d9e2b1d306ead1e
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-06 22:19:47 -0600

    Made App and Add Entry Form updates

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ function App() {
 
   const [entries, setEntries] = useState([{Index: '', name: 'Buddy the Elf', post: 'Merry Christmas', status: 'Thumbs Up'}, {index: '', name: 'Scrooge', post: 'Bah Humbug', status: 'Thumbs Down'}])
 
+  function addNewEntry(entry) {
+
+    let tempEntries = [...entries, entry];
+    
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries}/>
-      <AddEntryForm />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
     </div>
   );
 }
--- src/Components/AddEntry/AddEntryForm.jsx
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -6,6 +6,16 @@ const AddEntryForm = (props) => {
     const [name, setName] = useState('');
     const [post, setPost] = useState('');
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newEntry = {
+            name: name,
+            post: post
+        }
+        console.log(newEntry);
+        props.addNewEntryProperty(newEntry)
+    }
+
     return ( 
         <form>
             <label>Name</label>
@@ -13,8 +23,10 @@ const AddEntryForm = (props) => {
             <label>Post</label>
             <input type='paragraph' value={post} />
             <label>Status</label>
-            <button type='Thumbs Up' />
-            <button type='Thumbs Down' />
+            <button type='submit'>Add</button>
+            <label>Rank</label>
+            <button type='status'>Thumbs Up/Thumbs Down</button>
+            {/* <button type='submit'>Thumbs Down</button> */}
         </form>
      );
 }
